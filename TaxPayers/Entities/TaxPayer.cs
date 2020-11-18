1b3178d147d7a49c2d4a48820798f7252c491a9b
Rafael Lima <rafael_weblima@hotmail.com>
2020-11-18 17:06:26 -0300

    Criação projeto, criação da Superclasse do projeto TaxPayer

---
+++ TaxPayers/Entities/TaxPayer.cs
@@ -0,0 +1,17 @@
+﻿namespace TaxPayers.Entities {
+   abstract class TaxPayer {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer() {
+        }
+        public TaxPayer(string name, double anualIncome) {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+    }
+
+  
+}
---
+++ TaxPayers/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TaxPayers {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
