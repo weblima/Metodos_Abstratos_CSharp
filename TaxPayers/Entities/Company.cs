94e4a5be7df4cf0a4fa13db2ac26f4be65b9d6f0
Rafael Lima <rafael_weblima@hotmail.com>
2020-11-19 10:28:29 -0300

    Implementação da sub escrita do método Tax na classe Individual

---
+++ TaxPayers/Entities/Company.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TaxPayers.Entities {
+    class Company : TaxPayer {
+
+        public int NumberOfEmproyees { get; set; }
+
+        public Company() {
+
+        }
+        public Company(string name, double anualIncome, int numberOfEmproyees) : base(name, anualIncome) {
+            NumberOfEmproyees = numberOfEmproyees;
+        }
+
+        public override double Tax() {
+            return 1;
+        }
+    }
+}
---
+++ TaxPayers/Entities/Individual.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxPayers.Entities {
+    class Individual : TaxPayer {
+
+        public Double HealthExpenditures { get; set; }
+
+        public Individual() {
+
+        }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome) {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax() {
+
+            double IndividualTax;
+
+            if (AnualIncome < 20000.00) {
+                IndividualTax = (AnualIncome * 0.15);
+            }
+            else {
+                IndividualTax = (AnualIncome * 0.25);
+            }
+
+            if (HealthExpenditures > 0) {
+                IndividualTax -= (HealthExpenditures * 0.5);
+            }
+
+            return IndividualTax;
+        }
+    }
+}
